a78d8f1528e1fff32d87570466c0b988030e34b1
David Gaither <gaitherdb@gmail.com>
2021-08-12 19:24:53 -0400

    update middleware & help js files

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path');
+const { clog } = require('./middleware/clog');
+// const api = require('./routes/index.js');
+
 
 const PORT = process.env.port || 3001;
 const app = express();
 
+// app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+// app.use('/api', api);
+
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -18,5 +27,5 @@ app.get('/notes', (req, res) =>
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
