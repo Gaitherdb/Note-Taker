cd0fb3377f526b0aaa071cbe9c5fece3769883cb
David Gaither <gaitherdb@gmail.com>
2021-08-12 19:35:01 -0400

    add route note.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,33 +1,32 @@
-// const notes = require('express').Router();
-// const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-// const uuid = require('../helpers/uuid');
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
-// // GET Route for retrieving all the tips
-// tips.get('/', (req, res) => {
-//   console.info(`${req.method} request received for tips`);
-//   readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
-// });
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-// // POST Route for a new UX/UI tip
-// tips.post('/', (req, res) => {
-//   console.info(`${req.method} request received to add a tip`);
-//   console.log(req.body);
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
 
-//   const { username, topic, tip } = req.body;
+  const { title, text} = req.body;
 
-//   if (req.body) {
-//     const newTip = {
-//       username,
-//       tip,
-//       topic,
-//       tip_id: uuid(),
-//     };
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      tip_id: uuid(),
+    };
 
-//     readAndAppend(newTip, './db/tips.json');
-//     res.json(`Tip added successfully 🚀`);
-//   } else {
-//     res.error('Error in adding tip');
-//   }
-// });
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
 
-// module.exports = tips;
+module.exports = notes;
